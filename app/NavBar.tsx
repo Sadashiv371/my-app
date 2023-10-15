72c05dcd2e9eae04c70da137deb3caf68d1a710f
Sadashiv371 <sadashivbadal@gmail.com>
2023-10-15 16:46:01 +0530

    second-commit

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
+import {AiOutlineBug} from 'react-icons/ai'
 
 const NavBar = () => {
+    const links =[
+        { label : 'Dashboard',href: '/'},
+        { label : 'Issues', href: '/issues'},
+    
+    ]
   return (
-    <nav>
-        <Link href="/" >Logo</Link>
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/" ><AiOutlineBug  /></Link>
 
-        <ul>
-            <li><Link href="/">DASHBOARD</Link> </li>
-            <li><Link href="/issues">Issues</Link></li>
+        <ul className='flex space-x-6'>
+            {links.map(link => 
+            <Link
+             key={link.href} 
+            className='text-zinc-500 hover:text-zinc-800'
+             href={link.href}>{link.label}</Link>)}
+            
         </ul>
     </nav>
   )
